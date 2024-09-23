3f07ecd03cb32ae4d126c4332acd4446c85252be
Nazar Palamar <nazar.palamar@infineon.com>
2024-09-23 21:19:31 +0300

    Boards: cyw20829: add custom flashloaded with 64k of erase block size
    Add custom flashloaded (for openocd) with 64k of erase block size.
    Default one has 512, which cause MCUbootloader overwrite during flash
    primary application.
    
    Signed-off-by: Nazar Palamar <nazar.palamar@infineon.com>

--- boards/infineon/cyw920829m2evk_02/board.cmake
+++ boards/infineon/cyw920829m2evk_02/board.cmake
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 board_runner_args(openocd "--target-handle=TARGET.cm33")
+board_runner_args(openocd "--openocd-search=${ZEPHYR_HAL_INFINEON_MODULE_DIR}/zephyr/blobs/flashloader/TARGET_CYW920829M2EVK-02")
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 board_runner_args(jlink "--device=CYW20829_tm")
 include (${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
